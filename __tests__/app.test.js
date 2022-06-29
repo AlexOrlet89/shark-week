aea8c08c543b41be01c7701798fc613bf8aa9cb2
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-29 11:02:30 -0700

    get test added, controller route for getAll

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,19 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should return a baby shark', async () => {
+    const response = await request(app).get('/sharks');
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual([
+      {
+        id: '1',
+        scientific_name: 'Baby Shark',
+        family: 'Everyone',
+        kingdom: 'Philodendron',
+        living: 'yes',
+        random_fact: 'doot dooot doot',
+      },
+    ]);
   });
   afterAll(() => {
     pool.end();
