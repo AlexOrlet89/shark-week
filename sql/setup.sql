f912ae450425da59d1ffddaf2f5bf3d1dca8c1c7
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-29 09:50:11 -0700

    created Table but will pass over to brenden because he is finding and importing data

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS sharks;
+
+CREATE TABLE sharks (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    scientific_name TEXT NOT NULL,
+    family TEXT NOT NULL,
+    kingdom TEXT NOT NULL,
+    living TEXT NOT NULL,
+    random_fact TEEXT NOT NULL,
+
+    );
+-- CREATE TABLE userss (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     first_name TEXT NOT NULL,
+--     last_name TEXT NOT NULL,
+--     email TEXT NOT NULL,
+--     password_hash TEXT NOT NULL
+--     );
