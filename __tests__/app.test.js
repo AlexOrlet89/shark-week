5e5b9dee4f247f125002a50abd086aa5eada8bf6
BrendenSeifried <brendenseifried@gmail.com>
2022-06-30 11:50:44 -0700

    created test to confirm user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,11 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Shark = require('../lib/models/Shark');
-
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'Brenden@gmail.com',
   password: '12345',
+};
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, userProps });
+  const { email } = user;
+
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('sharks routes', () => {
@@ -93,6 +102,16 @@ describe('sharks routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
+    });
+  });
+
+  it('test to confirm current user is logged in', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
   afterAll(() => {
