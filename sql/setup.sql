e4a9a4504be3921d1161f6ce19ad731121b54531
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-30 10:00:40 -0700

    added users table to setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS sharks;
+DROP TABLE IF EXISTS users cascade;
 
 CREATE TABLE sharks (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -10,13 +11,12 @@ CREATE TABLE sharks (
     living TEXT NOT NULL,
     random_fact TEXT NOT NULL
     );
--- CREATE TABLE userss (
---     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     first_name TEXT NOT NULL,
---     last_name TEXT NOT NULL,
---     email TEXT NOT NULL,
---     password_hash TEXT NOT NULL
---     );
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+    );
 
 INSERT INTO sharks (scientific_name, family, kingdom, living, random_fact) VALUES
 ('Baby Shark', 'Everyone', 'Philodendron', 'yes', 'doot dooot doot');
